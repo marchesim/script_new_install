3adb130ad27409f0286d2bde4614dde052548bff
simone <simone.marchetti78@gmail.com>
2015-02-05 20:37:15 +0100

    Inizio

---
+++ newinstall.sh
@@ -0,0 +1,22 @@
+# Script per l'installazione di applicazioni utili
+#!/bin/bash
+sudo apt-get install ubuntu-restricted-extras p7zip -y
+sudo apt-get install openjdk-7-jre icedtea-7-plugin -y
+sudo apt-get install skype -y
+sudo apt-get install amule -y
+sudo apt-get install gimp -y
+sudo apt-get install openshot -y
+sudo apt-get install vlc -y
+sudo apt-get install nautilus-open-terminal -y
+sudo apt-get install nautilus-image-converter -y
+sudo apt-get install build-essential -y
+sudo apt-get install gparted -y
+sudo apt-get install ssh -y
+sudo apt-get install ruby -y
+sudo apt-get install arduino -y
+sudo apt-get install mysql-server -y
+sudo apt-get install apache2 libapache2-mod-php5 php5-mysql -y
+sudo apt-get install phpmyadmin -y
+sudo apt-get install git -y
+sudo apt-get install git-cola -y
+exit
